2e1bc1cf14c9256747c45430fb4a743397e2e55f
Zafar Matyakupov <zafar.matyakupov@yandex.com>
2021-06-11 13:22:04 +0500

    splash Screen

--- app/src/main/java/space/adebyat/adebyat/SplashScreenActivity.kt
+++ app/src/main/java/space/adebyat/adebyat/SplashScreenActivity.kt
@@ -1,23 +1,41 @@
 package space.adebyat.adebyat
 
+import android.animation.Animator
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import space.adebyat.adebyat.databinding.ActivitySplashScreenBinding
 import space.adebyat.adebyat.ui.MainActivity
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        Handler(Looper.getMainLooper()).postDelayed({
-            var intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish()
-        }, 1000)
 
+        supportActionBar?.hide()
+        binding.lottieView.setMaxProgress(0.6f)
+        binding.lottieView.addAnimatorListener(object: Animator.AnimatorListener {
+            override fun onAnimationStart(animation: Animator?) {
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+                finish()
+            }
+
+            override fun onAnimationCancel(animation: Animator?) {
+            }
+
+            override fun onAnimationRepeat(animation: Animator?) {
+            }
+        })
 
     }
 }
